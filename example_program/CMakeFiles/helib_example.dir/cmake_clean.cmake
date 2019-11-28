87ce69bc525237ed21275058344ae04f2c90b6b4
justin <haotian.weng@hotmail.com>
2019-11-29 02:02:08 +1100

    add seal program

---
+++ example_program/CMakeFiles/helib_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helib_example.dir/helib_example.cpp.o"
+  "helib_example"
+  "helib_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helib_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
